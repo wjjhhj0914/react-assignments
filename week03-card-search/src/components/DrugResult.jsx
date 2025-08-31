3bba7e903588a93c5bafc668ee09cddf58c214d8
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-31 19:35:05 +0900

    feat: 약 검색 결과 UI 구현
    - DrugResult에서 데이터 패칭(fetching)
    - FoodCard 컴포넌트 분리하여 카드 형태로 렌더링
    - 피해야 할 음식과 권장 음식 구분해서 출력

--- week03-card-search/src/components/DrugResult.jsx
+++ week03-card-search/src/components/DrugResult.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import FoodCard from './FoodCard'
 
 const API = 'http://localhost:4000/drugs'
 
@@ -36,7 +37,7 @@ export default function DrugResult({ term }) {
           null
 
         if (!matchedDrug) {
-          setError(new Error('검색 결과가 없습니다. 😢'))
+          setError(new Error('검색 결과가 없습니다.'))
           return
         }
 
@@ -69,19 +70,66 @@ export default function DrugResult({ term }) {
       <p
         role="alert"
         aria-live="assertive"
-        className="text-red-400 text-base mt-6"
+        className="text-red-400 text-base font-semibold mt-6"
       >
         {error.message}
       </p>
     )
   }
 
+  if (!drug) return null
+
+  const avoidFood = Array.isArray(drug.avoid) ? drug.avoid : []
+  const goodFood = Array.isArray(drug.good) ? drug.good : []
+
   return (
-    drug && (
-      <article className="mt-6 p-4 bg-white rounded-lg shadow">
-        <h2 className="text-xl font-bold">{drug.name}</h2>
-        <p className="text-slate-500">{drug.engName}</p>
-      </article>
-    )
+    <section className="mt-6 space-y-8">
+      <header>
+        <h2 className="text-md font-bold text-slate-500">
+          <span className="text-emerald-600 font-bold">{drug.name}</span>에 대한
+          검색 결과입니다.
+        </h2>
+      </header>
+      <div>
+        <h3 className="mb-3 text-3xl font-semibold text-red-600">
+          Foods to Avoid
+        </h3>
+        <div className="grid gap-3">
+          {avoidFood.length > 0 ? (
+            avoidFood.map((item, index) => (
+              <FoodCard
+                key={index}
+                emoji={'❌'}
+                name={item.name}
+                description={item.description}
+                bgColour="bg-red-50"
+              />
+            ))
+          ) : (
+            <p className="text-slate-500">피해야 할 음식 정보가 없습니다. 🤓</p>
+          )}
+        </div>
+      </div>
+      <div>
+        <h3 className="mb-3 text-3xl font-semibold text-green-700">
+          Beneficial Foods
+        </h3>
+        <div className="grid gap-3">
+          {goodFood.length > 0 ? (
+            goodFood.map((item, index) => (
+              <FoodCard
+                key={index}
+                emoji={'👩🏻‍⚕️'}
+                name={item.name}
+                description={item.description}
+                bgColour="bg-green-50"
+              />
+            ))
+          ) : (
+            <p className="text-slate-500">권장되는 음식 정보가 없습니다. 🙂</p>
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
