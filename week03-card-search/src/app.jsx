c4de3c8b5e194bf283fd1d6f43662828d7543967
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-31 11:54:32 +0900

    chore: 불필요한 폴더 삭제 및 App 초기화
    - demo 폴더
    - utils 폴더
    - learn-section 폴더

--- week03-card-search/src/app.jsx
+++ week03-card-search/src/app.jsx
@@ -1,10 +1,7 @@
-import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
-
 export default function App() {
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
-    </LearnSection>
+    <main>
+      <h1>DrugMate</h1>
+    </main>
   )
 }
--- week03-card-search/src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as wait } from './wait'
-export { default as measureTime } from './measure-time'
-export { default as tw } from './tw'
